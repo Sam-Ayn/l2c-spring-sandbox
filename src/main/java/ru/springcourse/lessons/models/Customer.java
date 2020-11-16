4170a2e3126cf59ddef5a162c3806158257ced91
Sam-Ayn <samueleayn@gmail.com>
2020-11-16 21:50:18 +0300

    Spring MVC Form Validation - Validating Number Ranges and Regular Expressions

--- src/main/java/ru/springcourse/lessons/models/Customer.java
+++ src/main/java/ru/springcourse/lessons/models/Customer.java
@@ -1,7 +1,6 @@
 package ru.springcourse.lessons.models;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 public class Customer {
 
@@ -10,6 +9,15 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName;
+
+    @NotNull(message = "is required")
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value = 10, message = "must be less than or equal to ten")
+    private Integer freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 characters/digits")
+    private String postalCode;
+
 
     public String getFirstName() {
         return firstName;
@@ -25,5 +33,21 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(Integer freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
